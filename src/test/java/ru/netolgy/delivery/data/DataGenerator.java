ca4e30700fd190e1c08fa2d54d45c1520d4e98aa
Vladimir <vladimir.zhevagin@yandex.ru>
2022-01-05 16:32:33 +0300

    allure

--- src/test/java/ru/netolgy/delivery/data/DataGenerator.java
+++ src/test/java/ru/netolgy/delivery/data/DataGenerator.java
@@ -15,29 +15,21 @@ public class DataGenerator {
     public static String generateDate(int shift) {
         LocalDate date = LocalDate.now().plusDays(shift);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
         return date.format(formatter);
     }
 
     public static String generateCity(String locale) {
         Faker faker = new Faker(new Locale(locale));
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
         return faker.address().city();
     }
 
     public static String generateName(String locale) {
         Faker faker = new Faker(new Locale(locale));
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
         return faker.name().name();
     }
 
     public static String generatePhone(String locale) {
         Faker faker = new Faker(new Locale(locale));
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
         return faker.phoneNumber().phoneNumber();
     }
 
@@ -50,8 +42,6 @@ public class DataGenerator {
         }
 
         public static UserInfo generateUser(String locale) {
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
             return new UserInfo(
                     generateCity(locale),
                     generateName(locale),
